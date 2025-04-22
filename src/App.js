2c8d7427ed28e5edee79e859ccf197b6a7b460f8
Nikita Kov <Kov_nikita2003@mail.ru>
2025-04-22 15:25:06 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import BootstrapSite from './components/BootstrapVersion';
 // import TailwindSite from './components/TailwindVersion';
 import AntDesignSite from './components/AntDesignVersion';
 
 function App() {
   return (
-	<BootstrapSite/>
-	// <TailwindSite/>
-	// <AntDesignSite/>
+    <Router basename={process.env.PUBLIC_URL} >
+      <Routes>
+        <Route path="/" element={<BootstrapSite />} />
+        <Route path="/antdisignversion" element={<AntDesignSite />} />
+      </Routes>
+    </Router>
   );
 }
 
